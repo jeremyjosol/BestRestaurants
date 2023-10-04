2221764c3ebbcfcf72da4ddafa3a160e2a35b68e
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-04 10:26:18 -0700

    Create CuisinesController with Index action

---
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using BestRestaurants.Models;
+
+namespace BestRestaurants.Controllers
+{
+  public class CuisinesController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+    public CuisinesController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Cuisine> model = _db.Cuisines.ToList();
+      return View(model);
+    }
+  }
+}
