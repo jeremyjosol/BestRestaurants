9c7bf9b36290a349fdb237bebaeca84a8453627f
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-04 12:34:35 -0700

    Bug resolved- change IsMichelin datatype to TINYINT(1)

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySqlConnector;
 
 namespace BestRestaurants.Models
 {
@@ -7,7 +8,7 @@ namespace BestRestaurants.Models
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public int Rating { get; set; }
-    public bool IsMichelin { get; set; }
+    public bool? IsMichelin { get; set; }
     public int CuisineId { get; set; }
     public Cuisine Cuisine { get; set; }
   }
