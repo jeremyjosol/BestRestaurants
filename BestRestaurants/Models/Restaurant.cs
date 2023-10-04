df8163c952f5c1da322effe997566e2d2d1416d5
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-04 10:21:22 -0700

    Create Restaurant class + establish relationship with Cuisine

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public int Rating { get; set; }
+    public bool IsMichelin { get; set; }
+    public int CuisineId { get; set; }
+    public Cuisine Cuisine { get; set; }
+  }
+}
